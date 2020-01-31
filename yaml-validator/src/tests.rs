da2df40bbb2a99c6ddda15013faf755f46b672ae
Mathias Pius <contact@pius.io>
2020-01-31 16:28:49 +0100

    Duplicated tests for schemastring

--- yaml-validator/src/tests.rs
+++ yaml-validator/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{error::SchemaErrorKind, SchemaObject};
+use crate::{error::SchemaErrorKind, SchemaObject, SchemaString};
 use std::convert::TryFrom;
 use yaml_rust::{Yaml, YamlLoader};
 
@@ -6,46 +6,97 @@ fn load_simple(source: &'static str) -> Yaml {
     YamlLoader::load_from_str(source).unwrap().remove(0)
 }
 
-#[test]
-fn schemaobject_from_yaml() {
-    SchemaObject::try_from(&load_simple(
-        "items:\n  - name: something\n    type: string",
-    ))
-    .unwrap();
+
+mod schemaobject {
+    use super::*;
+    #[test]
+    fn schemaobject_from_yaml() {
+        SchemaObject::try_from(&load_simple(
+            "items:\n  - name: something\n    type: string",
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn schemaobject_from_string() {
+        assert_eq!(
+            SchemaObject::try_from(&load_simple("world")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "string"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn schemaobject_from_integer() {
+        assert_eq!(
+            SchemaObject::try_from(&load_simple("10")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "integer"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn schemaobject_from_array() {
+        assert_eq!(
+            SchemaObject::try_from(&load_simple("- hello\n  - world")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "array"
+            }
+            .into()
+        );
+    }
 }
 
-#[test]
-fn schemaobject_from_string() {
-    assert_eq!(
-        SchemaObject::try_from(&load_simple("world")).unwrap_err(),
-        SchemaErrorKind::WrongType {
-            expected: "hash",
-            actual: "string"
-        }
-        .into()
-    );
-}
+mod schemastring {
+    use super::*;
+    #[test]
+    fn schemastring_from_yaml() {
+        SchemaString::try_from(&load_simple(
+            "type: string",
+        ))
+        .unwrap();
+    }
 
-#[test]
-fn schemaobject_from_integer() {
-    assert_eq!(
-        SchemaObject::try_from(&load_simple("10")).unwrap_err(),
-        SchemaErrorKind::WrongType {
-            expected: "hash",
-            actual: "integer"
-        }
-        .into()
-    );
-}
+    #[test]
+    fn schemastring_from_string() {
+        assert_eq!(
+            SchemaString::try_from(&load_simple("world")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "string"
+            }
+            .into()
+        );
+    }
 
-#[test]
-fn schemaobject_from_list() {
-    assert_eq!(
-        SchemaObject::try_from(&load_simple("- hello\n  - world")).unwrap_err(),
-        SchemaErrorKind::WrongType {
-            expected: "hash",
-            actual: "array"
-        }
-        .into()
-    );
+    #[test]
+    fn schemastring_from_integer() {
+        assert_eq!(
+            SchemaString::try_from(&load_simple("10")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "integer"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn schemastring_from_array() {
+        assert_eq!(
+            SchemaString::try_from(&load_simple("- hello\n  - world")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "array"
+            }
+            .into()
+        );
+    }
 }
