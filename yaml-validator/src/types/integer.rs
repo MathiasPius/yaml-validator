fd9c50423b081e14a4416778cd8faf86f5eece24
Mathias Pius <contact@pius.io>
2020-02-22 17:04:47 +0100

    Implemented maximum, exclusiveMaximum, minimum, exclusiveMinimum for integer type

--- yaml-validator/src/types/integer.rs
+++ yaml-validator/src/types/integer.rs
@@ -1,18 +1,64 @@
-use crate::error::SchemaError;
-use crate::utils::YamlUtils;
+use crate::error::{optional, SchemaError, SchemaErrorKind};
+use crate::utils::{Limit, YamlUtils};
 use crate::{Context, Validate};
 use std::convert::TryFrom;
 use yaml_rust::Yaml;
 
 #[derive(Debug, Default)]
-pub(crate) struct SchemaInteger {}
+pub(crate) struct SchemaInteger {
+    minimum: Option<Limit<i64>>,
+    maximum: Option<Limit<i64>>,
+}
 
 impl<'schema> TryFrom<&'schema Yaml> for SchemaInteger {
     type Error = SchemaError<'schema>;
     fn try_from(yaml: &'schema Yaml) -> Result<Self, Self::Error> {
-        yaml.strict_contents(&[], &["type"])?;
+        yaml.strict_contents(
+            &[],
+            &[
+                "type",
+                "minimum",
+                "exclusiveMinimum",
+                "maximum",
+                "exclusiveMaximum",
+            ],
+        )?;
 
-        Ok(SchemaInteger {})
+        yaml.check_exclusive_fields(&["minimum", "exclusiveMinimum"])?;
+        yaml.check_exclusive_fields(&["maximum", "exclusiveMaximum"])?;
+
+        let minimum = yaml
+            .lookup("minimum", "integer", Yaml::as_i64)
+            .map(Limit::Inclusive)
+            .map(Option::from)
+            .or_else(optional(None))?
+            .or(yaml
+                .lookup("exclusiveMinimum", "integer", Yaml::as_i64)
+                .map(Limit::Exclusive)
+                .map(Option::from)
+                .or_else(optional(None))?);
+
+        let maximum = yaml
+            .lookup("maximum", "integer", Yaml::as_i64)
+            .map(Limit::Inclusive)
+            .map(Option::from)
+            .or_else(optional(None))?
+            .or(yaml
+                .lookup("exclusiveMaximum", "integer", Yaml::as_i64)
+                .map(Limit::Exclusive)
+                .map(Option::from)
+                .or_else(optional(None))?);
+
+        if let (Some(lower), Some(upper)) = (&minimum, &maximum) {
+            if !lower.has_span(&upper) {
+                return Err(SchemaErrorKind::MalformedField {
+                    error: "range given for real value spans 0 possible values".into(),
+                }
+                .into());
+            }
+        }
+
+        Ok(SchemaInteger { minimum, maximum })
     }
 }
 
@@ -22,7 +68,27 @@ impl<'yaml, 'schema: 'yaml> Validate<'yaml, 'schema> for SchemaInteger {
         _: &'schema Context<'schema>,
         yaml: &'yaml Yaml,
     ) -> Result<(), SchemaError<'yaml>> {
-        yaml.as_type("integer", Yaml::as_i64).and_then(|_| Ok(()))
+        let value = yaml.as_type("integer", Yaml::as_i64)?;
+
+        if let Some(minimum) = &self.minimum {
+            if !minimum.is_greater(&value) {
+                return Err(SchemaErrorKind::ValidationError {
+                    error: "value violates lower limit constraint",
+                }
+                .into());
+            }
+        }
+
+        if let Some(maximum) = &self.maximum {
+            if !maximum.is_lesser(&value) {
+                return Err(SchemaErrorKind::ValidationError {
+                    error: "value violates upper limit constraint".into(),
+                }
+                .into());
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -80,6 +146,51 @@ mod tests {
     }
 
     #[test]
+    fn with_exclusive_conflict() {
+        assert_eq!(
+            SchemaInteger::try_from(&load_simple(
+                r#"
+                type: integer
+                exclusiveMinimum: 10
+                minimum: 10
+            "#)).unwrap_err(),
+            SchemaErrorKind::MalformedField {
+                error: "conflicting constraints: minimum, exclusiveMinimum cannot be used at the same time".into()
+            }.into()
+        )
+    }
+
+    #[test]
+    fn with_mixed_limits() {
+        SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                exclusiveMaximum: 12
+                minimum: 10
+            "#,
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn with_zero_width_limits() {
+        assert_eq!(
+            SchemaInteger::try_from(&load_simple(
+                r#"
+                type: real
+                exclusiveMinimum: 10
+                exclusiveMaximum: 11
+            "#
+            ))
+            .unwrap_err(),
+            SchemaErrorKind::MalformedField {
+                error: "range given for real value spans 0 possible values".into()
+            }
+            .into()
+        )
+    }
+
+    #[test]
     fn validate_string() {
         let schema = SchemaInteger::default();
 
@@ -115,6 +226,152 @@ mod tests {
             SchemaErrorKind::WrongType {
                 expected: "integer",
                 actual: "real"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_narrow_inclusive_set() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                minimum: 10
+                maximum: 10
+            "#,
+        ))
+        .unwrap();
+
+        schema
+            .validate(&Context::default(), &load_simple("10"))
+            .unwrap();
+    }
+
+    #[test]
+    fn validate_narrow_set() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                exclusiveMinimum: 10
+                maximum: 11
+            "#,
+        ))
+        .unwrap();
+
+        schema
+            .validate(&Context::default(), &load_simple("11"))
+            .unwrap();
+    }
+
+    #[test]
+    fn validate_min_exclusion() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                exclusiveMinimum: 10
+            "#,
+        ))
+        .unwrap();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("10"))
+                .unwrap_err(),
+            SchemaErrorKind::ValidationError {
+                error: "value violates lower limit constraint".into()
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_min_valid() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: real
+                minimum: 10
+            "#,
+        ))
+        .unwrap();
+
+        schema
+            .validate(&Context::default(), &load_simple("10"))
+            .unwrap();
+    }
+
+    #[test]
+    fn validate_min_invalid() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                minimum: 10
+            "#,
+        ))
+        .unwrap();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("5"))
+                .unwrap_err(),
+            SchemaErrorKind::ValidationError {
+                error: "value violates lower limit constraint".into()
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_max_exclusion() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                exclusiveMaximum: 10
+            "#,
+        ))
+        .unwrap();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("10"))
+                .unwrap_err(),
+            SchemaErrorKind::ValidationError {
+                error: "value violates upper limit constraint".into()
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_max_valid() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                maximum: 10
+            "#,
+        ))
+        .unwrap();
+
+        schema
+            .validate(&Context::default(), &load_simple("10"))
+            .unwrap();
+    }
+
+    #[test]
+    fn validate_max_invalid() {
+        let schema = SchemaInteger::try_from(&load_simple(
+            r#"
+                type: integer
+                maximum: 10
+            "#,
+        ))
+        .unwrap();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("20"))
+                .unwrap_err(),
+            SchemaErrorKind::ValidationError {
+                error: "value violates upper limit constraint".into()
             }
             .into()
         );
