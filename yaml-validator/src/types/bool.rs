fe36b51ff9a2b2d48c1fa207f72f8d98e60d2984
vk <vk@dbinvent.com>
2021-06-06 16:33:41 -0500

    unit tests added

--- yaml-validator/src/types/bool.rs
+++ yaml-validator/src/types/bool.rs
@@ -12,7 +12,7 @@ pub(crate) struct SchemaBool {
 impl<'schema> TryFrom<&'schema Yaml> for SchemaBool {
     type Error = SchemaError<'schema>;
     fn try_from(yaml: &'schema Yaml) -> Result<Self, Self::Error> {
-
+        yaml.strict_contents(&[], &["type"])?;
         Ok(SchemaBool {
         })
     }
@@ -37,6 +37,120 @@ mod tests {
     use crate::error::SchemaErrorKind;
     use crate::utils::load_simple;
     use crate::SchemaString;
+    use crate::types::SchemaInteger;
+
+    #[test]
+    fn from_yaml() {
+        SchemaBool::try_from(&load_simple("type: bool")).unwrap();
+    }
+
+    #[test]
+    fn from_string() {
+        assert_eq!(
+            SchemaString::try_from(&load_simple("true")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "boolean"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn from_integer() {
+        assert_eq!(
+            SchemaInteger::try_from(&load_simple("true")).unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "boolean"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn from_array() {
+        assert_eq!(
+            SchemaBool::try_from(&load_simple(
+                r#"
+                - hello
+                - world
+            "#
+            ))
+            .unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "hash",
+                actual: "array"
+            }
+            .into()
+        );
+    }
+
+
+    #[test]
+    fn validate_string() {
+        let schema = SchemaBool::default();
+        schema
+            .validate(&Context::default(), &load_simple("true"))
+            .unwrap();
+    }
+
+    #[test]
+    fn validate_integer() {
+        let schema = SchemaBool::default();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("10"))
+                .unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "bool",
+                actual: "integer"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_array() {
+        let schema = SchemaBool::default();
+
+        assert_eq!(
+            schema
+                .validate(
+                    &Context::default(),
+                    &load_simple(
+                        r#"
+                        - abc
+                        - 123
+                    "#
+                    )
+                )
+                .unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "bool",
+                actual: "array"
+            }
+            .into()
+        );
+    }
+
+    #[test]
+    fn validate_hash() {
+        let schema = SchemaBool::default();
+
+        assert_eq!(
+            schema
+                .validate(&Context::default(), &load_simple("hello: true"))
+                .unwrap_err(),
+            SchemaErrorKind::WrongType {
+                expected: "bool",
+                actual: "hash"
+            }
+            .into()
+        );
+    }
+
 
 
 }
