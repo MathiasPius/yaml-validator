35dc688d815ea650cd4264e37a11c63c06ece403
Mathias Pius <contact@pius.io>
2022-02-27 15:38:57 +0100

    Create ValidationError

---
+++ yaml-validator/src/errors/validation.rs
@@ -0,0 +1,87 @@
+use thiserror::Error;
+
+use crate::breadcrumb::{Breadcrumb, BreadcrumbSegment, BreadcrumbSegmentVec};
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum ValidationErrorKind<'a> {
+    #[error("wrong type, expected {expected} got {actual}")]
+    WrongType {
+        expected: &'static str,
+        actual: &'a str,
+    },
+    #[error("multiple validation errors were encountered: {errors:?}")]
+    Multiple { errors: Vec<ValidationError<'a>> },
+    #[error("unknown validation error")]
+    Unknown,
+}
+
+impl<'a> ValidationErrorKind<'a> {
+    pub fn with_path(self, path: BreadcrumbSegmentVec<'a>) -> ValidationError<'a> {
+        ValidationError {
+            kind: self,
+            state: Breadcrumb::new(path),
+        }
+    }
+
+    pub fn with_path_name(self, path: &'a str) -> ValidationError<'a> {
+        let mut err: ValidationError = self.into();
+        err.state.push(BreadcrumbSegment::Name(path));
+        err
+    }
+
+    pub fn with_path_index(self, index: usize) -> ValidationError<'a> {
+        let mut err: ValidationError = self.into();
+        err.state.push(BreadcrumbSegment::Index(index));
+        err
+    }
+}
+
+impl<'a> From<ValidationErrorKind<'a>> for ValidationError<'a> {
+    fn from(kind: ValidationErrorKind<'a>) -> ValidationError<'a> {
+        ValidationError {
+            kind,
+            state: Breadcrumb::default(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ValidationError<'a> {
+    pub kind: ValidationErrorKind<'a>,
+    pub state: Breadcrumb<'a>,
+}
+
+impl<'a> ValidationError<'a> {
+    fn flatten(&self, fmt: &mut std::fmt::Formatter<'_>, root: String) -> std::fmt::Result {
+        match &self.kind {
+            ValidationErrorKind::Multiple { errors } => {
+                for err in errors {
+                    err.flatten(fmt, format!("{}{}", root, self.state))?;
+                }
+            }
+            err => writeln!(fmt, "{}{}: {}", root, self.state, err)?,
+        }
+
+        Ok(())
+    }
+
+    pub fn add_path_name(path: &'a str) -> impl Fn(ValidationError<'a>) -> ValidationError<'a> {
+        move |mut err: ValidationError<'a>| -> ValidationError<'a> {
+            err.state.push(BreadcrumbSegment::Name(path));
+            err
+        }
+    }
+
+    pub fn add_path_index(index: usize) -> impl Fn(ValidationError<'a>) -> ValidationError<'a> {
+        move |mut err: ValidationError<'a>| -> ValidationError<'a> {
+            err.state.push(BreadcrumbSegment::Index(index));
+            err
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for ValidationError<'a> {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.flatten(fmt, "#".to_string())
+    }
+}
